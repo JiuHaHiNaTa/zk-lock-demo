dd07fe143f275dcc588fea9a094ad4f777e9568c
jiuha <jiuhamashiro@sina.com>
2022-03-23 10:46:10 +0800

    项目上传测试

---
+++ src/main/java/com/jiuha/zklockdemo/ZkLockDemoApplication.java
@@ -0,0 +1,13 @@
+package com.jiuha.zklockdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZkLockDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZkLockDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jiuha/zklockdemo/ZkLockDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiuha.zklockdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZkLockDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
